3ce2630bd5900039409642b8b8c501517c8a93ed
Nikita Nebolsin <nikita.nebolsin@gmail.com>
2023-03-04 19:41:56 +0300

    Add test.class, modify test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 class Test {
 	public static void main(String... args) {
 		System.out.println("First commit");
+		print("Hello Nick");
+	}
+	public static void print(String str) {
+		System.out.println(str);
 	}
 }
