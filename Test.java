65f6540c4f727ebf4fcce2bbfaffeb9a003ac2a6
Nikita Nebolsin <nikita.nebolsin@gmail.com>
2023-03-04 21:03:03 +0300

    add new comment

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 class Test {
 	public static void main(String... args) {
-		System.out.println("First commit");
+		// new comment
+		System.out.println("First commit, modify");
+		System.out.println("Second commit");
 		print("Hello Nick");
 	}
 	public static void print(String str) {
