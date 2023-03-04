b038723976beb97e659fa259c4be74f28b14af10
Nikita Nebolsin <nikita.nebolsin@gmail.com>
2023-03-04 23:30:20 +0300

    initial state

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 class Test {
 	public static void main(String... args) {
 		// new comment
-		// 4 commit
 		System.out.println("First commit, modify");
 		System.out.println("Second commit");
 		print("Hello Nick");
