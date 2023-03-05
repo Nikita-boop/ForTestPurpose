56e50cf08d050fb41820e721b58334560b3d2498
Nikita Nebolsin <nikita.nebolsin@gmail.com>
2023-03-05 12:21:28 +0300

    Add new string

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 class Test {
 	public static void main(String... args) {
 		// new comment
+		// for test path, amend?
 		System.out.println("First commit, modify");
 		System.out.println("Second commit");
 		print("Hello Nick");
