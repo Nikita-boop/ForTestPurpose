b7eaed883a525b89e011bf0dd03ee6d73028942c
Nikita Nebolsin <nikita.nebolsin@gmail.com>
2023-03-09 00:18:48 +0300

    kek

--- Test.java
+++ Test.java
@@ -1,11 +1,8 @@
 class Test {
 	public static void main(String... args) {
-<<<<<<< HEAD
 		// new comment
 		// from another folder
-=======
 		// new comments
->>>>>>> slave
 		// one more time
 		System.out.println("First commit, modify");
 		System.out.println("Second commit");
